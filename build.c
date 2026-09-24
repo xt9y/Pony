80dcc6cb882514d830604a5360653f6cb621ca82
xt9y <felix@xt9y.de>
2026-09-24 12:38:13 +0200

    Build split GPU initialization

--- build.c
+++ build.c
@@ -10,6 +10,7 @@ void build(C_Build *b) {
     c_sources(app, "bvh.c");
     c_sources(app, "lmap.c");
     c_sources(app, "fx_vision.c");
+    c_sources(app, "gpu.c");
     c_sources(app, "render.c");
     c_sources(app, "cache.c");
     c_sources(app, "beam.c");
